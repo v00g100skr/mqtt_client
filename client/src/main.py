3ca355e846e87820794eabf8d6bfe3e54aec25f1
v00g100skr <utmcrow@gmail.com>
2022-09-03 00:40:40 +0300

    separate containers

--- client/src/main.py
+++ client/src/main.py
@@ -1,18 +1,30 @@
 import paho.mqtt.client as mqtt
 
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("../src/debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
 # This is the Subscriber
 
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    logging.info("Connected with result code " + str(rc))
     client.subscribe("zigbee2mqtt_lan/geiger_counter")
 
 
 def on_message(client, userdata, msg):
     data = str(msg.payload.decode("utf-8"))
-    print("received message: ", data)
+    logging.info("received message: %s" % data)
 
-print("start")
+
+logging.info('start main')
 client = mqtt.Client()
 client.username_pw_set("zigstar", "zigstar")
 client.connect("10.2.0.40", 1883, 60)
